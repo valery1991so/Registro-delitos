7a918fc8170508176ea0a06171235ed16750565e
Valeria <valery1991so@gmail.com>
2026-05-28 12:12:58 -0600

    actualizacion visual

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import streamlit as st
 from supabase import create_client
 
+st.set_page_config(
+    page_title="Registro de Delitos",
+    page_icon="🚨",
+    layout="centered"
+)
+
 url = "https://uxgvgumfqbvsknhdjjnu.supabase.co"
 key = "sb_publishable_uMTn1gbS1AX5MIbmTa8nrA_-i2e8a7s"
 
 supabase = create_client(url, key)
 
-st.title("Registro de Delitos")
-
-tipo = st.text_input("Tipo de delito")
+st.title("🚨Registro de Delitos")
+st.markdown("### Reporta incidentes de forma rápida y sencilla")
+tipo = st.selectbox(
+    "Tipo de delito",
+    ["Robo", "Asalto", "Vandalismo", "Violencia", "Otro"]
+)
 fecha = st.date_input("Fecha")
 
 if st.button("Guardar"):
@@ -20,4 +29,4 @@ if st.button("Guardar"):
 
     supabase.table("Delitos").insert(datos).execute()
 
-    st.success("Dato guardado")
+    st.success("✅ Reporte enviado correctamente")
