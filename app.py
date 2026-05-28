488271cd5c5eb68b378e0b4b1d793e2e51b28c0e
Valeria <valery1991so@gmail.com>
2026-05-28 12:42:54 -0600

    actualizacion visual

--- app.py
+++ app.py
@@ -11,19 +11,48 @@ url = "https://uxgvgumfqbvsknhdjjnu.supabase.co"
 key = "sb_publishable_uMTn1gbS1AX5MIbmTa8nrA_-i2e8a7s"
 
 supabase = create_client(url, key)
+st.set_page_config(
+    page_title="Registro de Delitos",
+    page_icon="🚨",
+    layout="centered"
+)
 
 st.title("🚨Registro de Delitos")
-st.markdown("### Reporta incidentes de forma rápida y sencilla")
-tipo = st.selectbox(
-    "Tipo de delito",
-    ["Robo", "Asalto", "Vandalismo", "Violencia", "Otro"]
-)
+st.markdown("### Reporta rápidamente lo ocurrido")
+st.divider()
+
+st.subheader("¿Qué deseas reportar?")
+tipo_delito = None
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("🚗 Robo de vehículo", use_container_width=True):
+        tipo_delito = "Robo de vehículo"
+
+    if st.button("📱 Robo de celular", use_container_width=True):
+        tipo_delito = "Robo de celular"
+
+    if st.button("🧱 Vandalismo", use_container_width=True):
+        tipo_delito = "Vandalismo"
+
+with col2:
+    if st.button("⚠️ Asalto", use_container_width=True):
+        tipo_delito = "Asalto"
+
+    if st.button("👤 Persona sospechosa", use_container_width=True):
+        tipo_delito = "Persona sospechosa"
+
+    if st.button("📍 Otro", use_container_width=True):
+        tipo_delito = "Otro"
+
+
 fecha = st.date_input("Fecha")
 
 if st.button("Guardar"):
 
     datos = {
-        "tipo_delito": tipo,
+        "tipo_delito": tipo_delito,
         "fecha": str(fecha)
     }
 
